11fda5e60f94f76bb3e8f7610856d32ba89199e3
ThisIsBelgium <Daniel3wheeler@gmail.com>
2017-07-11 16:07:21 -0500

    added map model and added functionality to the map controller

---
+++ ConCon/Models/MapViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConCon.Models
+{
+    public class MapViewModel
+    {
+        public ApplicationUser user { get; set; }
+        public List<object> events { get; set; }
+    }
+}
